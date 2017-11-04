cbf7ba897be0c2c153ab3851291d30d9cabf4e74
rudiaj <aj.rudi@gmail.com>
2017-11-04 21:58:50 +0100

    added webpack and react hello world

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from './App';
+// import {Provider} from "react-redux";
+// import AppComponent from "App.js";
+// import store from "./store";
+
+ReactDOM.render(<App/>,document.getElementById('app'));
+
